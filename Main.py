06b66b96763e4f7b590b72b5999c66b2b0f09ec9
jamjar2005 <jamesmhaven@gmail.com>
2022-11-17 12:35:32 -0800

    Added first 2 sources

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 '''
-Overview of this thing
+Using Pygame to create launch/thrust animations and a smoke trail, 
+along with a changing background based on altitude and an altimeter.
 
 '''
 
 # sources:
-
+# for help with particles: https://www.youtube.com/watch?v=yfcsB3SGsKY (Clear code)
+# for help with background images: https://www.youtube.com/watch?v=WurCpmHtQc4 (Codemy.com)
 # import libraries
+import pygame as pg
 
 # built in
 
 # installed modules or libraries
 
 # created modules or libraries
-import settings
+import settings 
 # global variables
 
-#
+# utilit
