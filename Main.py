4072fc41cc8475758e743b9559bbe6a840f4b2f3
jamjar2005 <jamesmhaven@gmail.com>
2022-11-28 11:18:59 -0800

    I defined colors and a colors array in settings.py

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ along with a changing background based on altitude and an altimeter.
 # sources:
 # for help with particles: https://www.youtube.com/watch?v=yfcsB3SGsKY (Clear code)
 # for help with background images: https://www.youtube.com/watch?v=WurCpmHtQc4 (Codemy.com)
+# for help with sprites: https://www.youtube.com/watch?v=hDu8mcAlY4E (Clear code)
 # import libraries
 import pygame as pg
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+# Defining colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 128, 0)
+CYAN = (0, 255, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Putting all defined colors into an array 
+COLORS = [RED, GREEN, BLUE, ORANGE, CYAN, WHITE, BLACK]
