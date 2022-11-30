d8a7380a9e1c6f88ea52298eec66bec8694efac3
jamjar2005 <jamesmhaven@gmail.com>
2022-11-30 09:03:22 -0800

    Added game settings and added game loop

--- Main.py
+++ Main.py
@@ -5,9 +5,11 @@ along with a changing background based on altitude and an altimeter.
 '''
 
 # sources:
+# Mr. Cozort's source code files
 # for help with particles: https://www.youtube.com/watch?v=yfcsB3SGsKY (Clear code)
 # for help with background images: https://www.youtube.com/watch?v=WurCpmHtQc4 (Codemy.com)
 # for help with sprites: https://www.youtube.com/watch?v=hDu8mcAlY4E (Clear code)
+
 # import libraries
 import pygame as pg
 
@@ -16,7 +18,24 @@ import pygame as pg
 # installed modules or libraries
 
 # created modules or libraries
-import settings 
+from settings import *
+
 # global variables
 
 # utilit
+
+# initializing pygame and creating a visible window
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Space Voyager")
+clock = pg.time.Clock()
+
+# game loop (while loop)
+running = True 
+while running:
+    #this keeps the game running using the clock
+    clock.tick(FPS)
+# drawing the background screen
+    screen.fill(BLACK)
+pg.quit()
--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+# game settings
+WIDTH = 1200
+HEIGHT = 750
+FPS = 30
+
 # Defining colors
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
